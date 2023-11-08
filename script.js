0739281e3e3bbfadc53229ac48f6cb94842ec250
Asiak <asiakemp24@gmail.com>
2023-11-08 18:21:45 -0500

    pseudocoded

---
+++ script.js
@@ -0,0 +1,12 @@
+// define all variable and accessing the HTML elements
+
+// track lists
+
+// configure the player buttons
+
+// slider to know what the time for the song is
+
+// volume slider
+
+// starting the music player
+
